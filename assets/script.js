6d1d915debeefbb44a0c304bb7ba396d109288fc
byronallan <127366720+byronallan@users.noreply.github.com>
2023-04-07 23:34:19 -0700

    refactored code again, still need to define passsword

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,55 @@
 // Assignment Code
-
-// This variable targets the button element in the html due to the fact it includes #generate. 
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("The button is working when I press it.");
   
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numChar = "0123456789";
-  var specChar = "!@#$%^&*()-_=+,<.>/?";
-  
-  var result = "";
+  var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var numChar = ['0','1','2','3','4','5','6','7','8','9'];
+  var specChar = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+',',','<','.','>','/','?'];
+  var selectChar = [];
+
   var length = window.prompt("Please choose a password length of at least 8 characters and no more than 128 characters"); 
-  
+
   var passwordLength = parseInt(length);
-  
+
   while (passwordLength >= 8 && passwordLength <= 128 && isFinite) {
+    console.log("Woohoo! The written password length has met the criteria!")
     confirm("Would you like to include lowerCase characters in your password?"); {
-      lowerCase += result
+      console.log("Hurray! Lowercase letters have been added to the password criteria!")
+      if (lowerCase) {
+        selectChar += lowerCase;
+      }
     }
     confirm("Would you like to use upperCase characters?"); {
-      upperCase += result
+      console.log("Yay! Uppercase letters have been added to the password criteria!")
+      if (upperCase) {
+        selectChar += upperCase;
+      }
     }
     confirm("Would you like to include numeric characters in your password?"); {
-      numChar += result
+      console.log("Cheers! Numerics have been added to the password criteria!")
+      if (numChar) {
+        selectChar += numChar;
+      }
     }
     confirm("Would you like to include special characters in your password?"); {
-      specChar += result
-    }
-    if (lowerCase || upperCase || numChar || specChar) {
-      for (var i = 0; i < passwordLength; i++) {
-        password += result.charAt(Math.floor(Math.random() * result.length))
+      console.log("Whoopee! Special characters have been added to the password criteria!")
+      if (selectChar) {
+        selectChar += specChar;
       }
-      return password 
+    }
+    if (lowerCase == true || upperCase == true || numChar == true || specChar == true); {
+    }
+    console.log(selectChar);
+    // This break was made to end the while loop if the criteria for the prompt was determined false.
+    break; 
     } 
-  break; 
-  }  
-  alert("The password must be a number between 8-128 characters...come on now."); {
-    return "Error!"
+    console.log("Boohoo! The criteria was not met... :(")
+      alert("The password must be a number between 8-128 characters...come on now."); {
+      return "Error!"; {
+    }
   }
 }
 
@@ -46,32 +57,10 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-//   var lowerCase = confirm("Would you like to include lowerCase characters in your password?")
-//   if(confirm) {
-//     result += lowerCase
-//   }
-//   var upperCase = confirm("Would you like to use upperCase characters?")
-//   if(confirm) {
-//     result += upperCase
-//   }
-//   var numbers = confirm("Would you like to include numeric characters in your password?")
-//   if(confirm) {
-//     result += numbers
-//   }
-//   var specChar = confirm("Would you like to include special characters in your password?")
-//   if(confirm) {
-//     result += specChar
-//   }
-//   if (lowerCase === false && upperCase === false && numeric === false && specialCharacters === false) {
-//     alert("The criteria requires a character type, please choose at least one!");
-//     console.log(result);
-//   }
-// }
-// generatePassword();
